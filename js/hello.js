92359d78778b48d81436bd92f39bcd00cad15392
tlsaudwl <tlsaudwl1821@naver.com>
2024-01-25 09:13:12 +0900

    tailwind 실습

---
+++ js/hello.js
@@ -0,0 +1,6 @@
+// primitive (call-by-value) vs. objective (call-by-reference) -> 면접 질문으로 잘 나옴
+const userName = 'Hong'; // string (primitive)
+let age; // declare + define -> 이 정의는 메모리가 할당되었다는 뜻 (undefined라는 값이 저장됨)
+age=33;
+console.log("🚀 ~ age:", age)
+console.log(`Hello, ${userName}!`);
