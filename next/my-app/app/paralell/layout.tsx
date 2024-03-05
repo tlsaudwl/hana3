1b536ab26ae41027eb68fa582837e3fd56d41a0b
tlsaudwl <tlsaudwl1821@naver.com>
2024-03-05 15:11:35 +0900

    Next 3/5-2

--- next/my-app/app/paralell/layout.tsx
+++ next/my-app/app/paralell/layout.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Link from "next/link";
+import { useReducer } from "react";
 
 // app/paralell/layout.tsx
 export default function ParalellLayout({
@@ -10,18 +13,32 @@ export default function ParalellLayout({
   profile: React.ReactNode;
   login: React.ReactNode;
 }) {
+  // const isLogin = false;
+  const [isLogin, toggleLogin] = useReducer((pre) => !pre, true);
   return (
     <>
-      <h1>ParalellLayout</h1>
-      <div className="border border-dotted border-red-500 flex justify-around p-5">
-        <div className="border p-3">{profile}</div>
-        <div className="border p-3">{login}</div>
-      </div>
+      <h1>
+        ParalellLayout
+        <button onClick={toggleLogin} className="float-end text-blue-500">
+          Toggle Login
+        </button>
+      </h1>
+      {isLogin ? (
+        <div className="border border-dotted border-red-500 flex justify-around p-5">
+          <div className="border p-3">{profile}</div>
+          <div className="border p-3">{login}</div>
+        </div>
+      ) : (
+        login
+      )}
+
       {children}
+
       <div className="flex justify-around">
         <Link href="/paralell/bbb">Profile/BBB</Link>
         <Link href="/paralell/aaa">Login/AAA</Link>
       </div>
+
       <div className="flex justify-around">
         <Link href="/paralell/ccc">Profile/CCC</Link>
         <Link href="/paralell/ddd">Login/DDD</Link>
