40309293d4e045e65eb91b9ac359c32c042356d7
tlsaudwl <tlsaudwl1821@naver.com>
2024-03-06 17:33:45 +0900

    Next 3/6-1

---
+++ next/my-app/app/photos/[photoId]/page.tsx
@@ -0,0 +1,3 @@
+export default function PhotoDetail({ params }: { params: { photoId: string } }) {
+  return <>{params.photoId}</>;
+}
---
+++ next/my-app/app/photos/layout.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+
+export default function PhotosLayout({ children, viewer }: { children: ReactNode; viewer: ReactNode }) {
+  return (
+    <>
+      {viewer}
+      {children}
+    </>
+  );
+}
